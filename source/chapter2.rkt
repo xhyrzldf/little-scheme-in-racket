dbeb79b43e55bab9d28a8492c488c22b47b45804
xhyrzldf <296796683@qq.com>
2018-03-26 23:58:21 +0800

    member function finish

--- source/chapter2.rkt
+++ source/chapter2.rkt
@@ -12,7 +12,17 @@
         (else #f))) ;;最后的问题直接 返回#f (else的值为#t)
 
 ;; 测试
-(lat? '(a c d f))
-(lat? '(jack rose (yoda luke)))
+(lat? '(a c d f)) ;; Expect #t
+(lat? '(jack rose (yoda luke))) ;;Expect #f
 
+;;十戒 1.在表述任意函数时，总是将询问null?作为诸问题之首
 
+;; member函数定义 用于判断一个列表是否包含某一个原子
+(define (member? a lat) 
+    (cond 
+        ((null? lat) #f) ;;如果lat是空列表 直接返回 #f
+        ((eq? a (car lat)) #t) ;; 查看lat的第一个元素是否等于 a
+        (else (member? a (cdr lat))))) ;; 判断剩下的元素是否包含 a
+
+;; 测试
+(member? 'meat '(mashed potatoes and meat gravy)) ;; Expect #t
